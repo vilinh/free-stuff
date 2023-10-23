8720d5f233329f1910021bbbbf1cf20cac2b35ab
KimLinhVu <klvbubble@gmail.com>
2023-10-23 13:02:23 -0700

    connect to mongodb atlas

--- backend/backend.js
+++ backend/backend.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_URL).then(
+  () => {
+    console.log("Connected to server successfully!");
+  }
+);
