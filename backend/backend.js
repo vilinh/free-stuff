97d342a10362423b35a07c446733314afbf0f5b9
vilinh <vilinhlvu@gmail.com>
2023-10-23 11:47:23 -0700

    set up backend server

--- backend/backend.js
+++ backend/backend.js
@@ -1,6 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
