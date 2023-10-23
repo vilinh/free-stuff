5bf85b756c549bb3e2e8964df97384b3d453d5cf
KimLinhVu <klvbubble@gmail.com>
2023-10-23 11:56:03 -0700

    set up mongodb atlas

--- backend/backend.js
+++ backend/backend.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_URL).then(
+  () => {
+    console.log("Connected to server successfully!");
+  }
+);
