f6dae960963e0b2db9141ecb52cdf2569a1fb15e
KimLinhVu <klvbubble@gmail.com>
2023-10-25 10:58:53 -0700

    added routers to backend for endpoints

--- backend/backend.js
+++ backend/backend.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import listing from "./routes/listing.js"
+import user from "./routes/user.js"
 
 dotenv.config();
 
@@ -11,8 +13,15 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
+app.use('/listing', listing)
+app.use('/user', user)
+
 mongoose.connect(process.env.DATABASE_URL).then(
   () => {
     console.log("Connected to server successfully!");
   }
 );
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
