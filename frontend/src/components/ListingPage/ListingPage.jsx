c569ab7bde9f09a1b4459156f0e221a31df806f5
franktucci <ftucci@calpoly.edu>
2023-11-02 16:35:09 -0700

    support for query params in listings view

--- frontend/src/components/ListingPage/ListingPage.jsx
+++ frontend/src/components/ListingPage/ListingPage.jsx
@@ -1,20 +1,22 @@
 import { ListingPanel } from "../ListingPanel/ListingPanel";
 import { UserPanel } from "../UserPanel/UserPanel";
 import { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
 import axios from "axios";
 import "./ListingPage.css";
 
 export const ListingPage = ({ listing }) => {
   const [listings, setListings] = useState([])
+  const location = useLocation();
 
   useEffect(() => {
     const getListings = async () => {
       try {
-				let res = await axios.get("http://localhost:8000/listing");
-				setListings(res.data);
-			} catch (error) {
-				console.log(error);
-			}
+        let res = await axios.get("http://localhost:8000/listing" + location.search);
+        setListings(res.data);
+        } catch (error) {
+            console.log(error);
+        }
     }
     getListings();
   }, [])
