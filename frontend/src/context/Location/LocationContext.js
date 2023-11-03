4a6e849edd39b4bb85a791e4b37be50dbc640ac9
vilinh <vilinhlvu@gmail.com>
2023-11-03 11:12:47 -0700

    move getLocation to Context

---
+++ frontend/src/context/Location/LocationContext.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+const LocationContext = createContext();
+
+export function useLocationContext() {
+    return useContext(LocationContext);
+  }
+
+export function LocationProvider({ children }) {
+    const [address, setAddress] = useState("")
+
+    useEffect(() => {
+        async function getAddress(coords) {
+            const lat = coords.lat;
+            const lng = coords.lng;
+            const request = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+            try {
+              const res = await axios.get(request);
+              setAddress(res.data.results[0].formatted_address);
+            } catch (error) {
+              console.log("could not fetch address");
+            }
+          }
+      
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+              const latitude = position.coords.latitude;
+              const longitude = position.coords.longitude;
+              getAddress({ lat: latitude, lng: longitude });
+            });
+          }
+    }, [])
+
+    const value = {
+        address
+    }
+
+    return (
+        <LocationContext.Provider value={value}>
+          {children}
+        </LocationContext.Provider>
+      );
+
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./context/Auth/AuthContext";
+import { LocationProvider } from "./context/Location/LocationContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
-    <App />
+    <LocationProvider>
+       <App />
+    </LocationProvider>
   </AuthProvider>
 );
