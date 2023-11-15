9c5fa309e8023a36656400ad128fe2ef515daa44
KimLinhVu <klvbubble@gmail.com>
2023-11-15 08:59:37 -0800

    added prettier style to files

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,28 +1,28 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
-	{
-		uid: {
-			type: String,
-			required: true,
-		},
-		display_name: {
-			type: String,
-			default: "",
-		},
-		email: {
-			type: String,
-			required: true,
-		},
-		profile_pic: {
-			type: String,
-		},
-		claimed_listings: {
-			type: [String],
-			default: [],
-		},
-	},
-	{ collection: "users_list" }
+  {
+    uid: {
+      type: String,
+      required: true,
+    },
+    display_name: {
+      type: String,
+      default: "",
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    profile_pic: {
+      type: String,
+    },
+    claimed_listings: {
+      type: [String],
+      default: [],
+    },
+  },
+  { collection: "users_list" },
 );
 
 export default mongoose.model("User", UserSchema);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,81 +5,81 @@ const router = express.Router();
 
 // get user by firebase uid
 router.get("/:uid", async (req, res) => {
-	const uid = req.params["uid"];
-	let result = await findUserByUid(uid);
-	if (result == undefined || result.length == 0) {
-		res.status(404).send("Resource not found.");
-	} else {
-		res.status(200).send(result);
-	}
+  const uid = req.params["uid"];
+  let result = await findUserByUid(uid);
+  if (result == undefined || result.length == 0) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.status(200).send(result);
+  }
 });
 
 router.post("/", async (req, res) => {
-	const userToAdd = new userModel(req.body);
-	let result = await addUser(userToAdd);
-	if (result === undefined) {
-		res.status(500).send("An error occurred in the server.");
-	} else {
-		res.status(201).send(userToAdd);
-	}
+  const userToAdd = new userModel(req.body);
+  let result = await addUser(userToAdd);
+  if (result === undefined) {
+    res.status(500).send("An error occurred in the server.");
+  } else {
+    res.status(201).send(userToAdd);
+  }
 });
 
 router.put("/:id", async (req, res) => {
-	const id = req.params["id"];
-	const result = await updateUserById(id, req.body);
-	if (result === undefined) {
-		res.status(404).send("Resource not found.");
-	} else {
-		res.status(204).end();
-	}
+  const id = req.params["id"];
+  const result = await updateUserById(id, req.body);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.status(204).end();
+  }
 });
 
 // delete user by firebase uid
 router.delete("/:uid", async (req, res) => {
-	const uid = req.params["uid"];
-	let result = await removeUserByUid(uid);
-	if (result == undefined) {
-		res.status(404).send("Resource not found");
-	} else {
-		res.status(204).end();
-	}
+  const uid = req.params["uid"];
+  let result = await removeUserByUid(uid);
+  if (result == undefined) {
+    res.status(404).send("Resource not found");
+  } else {
+    res.status(204).end();
+  }
 });
 
 async function findUserByUid(uid) {
-	try {
-		return await userModel.findOne({ uid });
-	} catch (error) {
-		console.log(error);
-		return undefined;
-	}
+  try {
+    return await userModel.findOne({ uid });
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 }
 
 async function removeUserByUid(uid) {
-	try {
-		let result = await userModel.findOneAndDelete({ uid });
-		return result;
-	} catch (error) {
-		console.log(error);
-		return undefined;
-	}
+  try {
+    let result = await userModel.findOneAndDelete({ uid });
+    return result;
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 }
 
 async function addUser(user) {
-	try {
-		return await user.save();
-	} catch (error) {
-		console.log(error);
-		return undefined;
-	}
+  try {
+    return await user.save();
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 }
 
 async function updateUserById(id, user) {
-	try {
-		await userModel.findByIdAndUpdate(id, user);
-	} catch (error) {
-		console.log(error);
-		return undefined;
-	}
+  try {
+    await userModel.findByIdAndUpdate(id, user);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 }
 
 export default router;
