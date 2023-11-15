932e716f0896de7ccbafb936b788f9e705724f64
vilinh <vilinhlvu@gmail.com>
2023-11-14 17:36:41 -0800

    add option to set location or automatically find

--- frontend/src/components/Nav/NavBar.jsx
+++ frontend/src/components/Nav/NavBar.jsx
@@ -6,62 +6,149 @@ import { useAuth } from "../../context/Auth/AuthContext";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase";
 import Input from "@cloudscape-design/components/input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Modal from "@cloudscape-design/components/modal";
+import SpaceBetween from "@cloudscape-design/components/space-between";
+import Spinner from "@cloudscape-design/components/spinner";
+import Autocomplete from "react-google-autocomplete";
+import { LocationSVG } from "../../svgs/LocationSVG";
+import { useLocationContext } from "../../context/Location/LocationContext";
+import Icon from "@cloudscape-design/components/icon";
 
 export const NavBar = () => {
   const { currentUser } = useAuth();
   const [search, setSearch] = useState("");
+  const [locationPref, setLocationPref] = useState(false);
+  const [searchLoc, setSearchLoc] = useState("");
+
+  const { findLocation, setAddress, loading, address } = useLocationContext();
+
+  const handlePlaceSelected = (place) => {
+    const latitude = place.geometry.location.lat();
+    const longitude = place.geometry.location.lng();
+    const address = place.formatted_address;
+    const loc = {
+      address,
+      latitude,
+      longitude,
+    };
+    setSearchLoc(address);
+  };
+
+  const confirmPlaceSelected = () => {
+    setAddress(searchLoc);
+    setLocationPref(false);
+    setSearchLoc("");
+  };
+
+  const handleFindLocation = () => {
+    findLocation();
+    setSearchLoc("");
+  };
 
   return (
-    <div className="navbar">
-      <div className="navbar-l">
-        <Link className="link" to="/">
-          <h3 className="logo-text">Broke Blessings</h3>
-        </Link>
-        <Link className="link" to="/listing">
-          <p className="view-listings-link">View Listings</p>
-        </Link>
-      </div>
-      <div className="navbar-m">
-        <Input
-          onChange={({ detail }) => setSearch(detail.value)}
-          value={search}
-		  placeholder="Search"
-        />
-      </div>
-      <div className="navbar-r">
-        {currentUser ? (
-          <>
-            <Button onClick={() => signOut(auth)} variant="inline-link">
-              Sign Out
-            </Button>
-            <Link className="link" to="/user">
-              <Avatar
-                alt="profile pic"
-                src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+    <>
+      <Modal
+        onDismiss={() => setLocationPref(false)}
+        visible={locationPref}
+        header="Pick a location"
+      >
+        <SpaceBetween direction="vertical">
+          <div className="modal-contents">
+            Manually enter location or click the Find My Location button to do
+            it automatically.
+            <div className="manual-input">
+              <Autocomplete
+                style={{
+                  border: ".05rem solid",
+                  borderRadius: ".25rem",
+                  width: "80%",
+                  fontSize: "1rem",
+                  padding: ".5rem",
+                }}
+                apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+                onPlaceSelected={handlePlaceSelected}
+                options={{
+                  types: ["address"],
+                }}
+                onChange={() => {
+                  setSearchLoc("");
+                }}
               />
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link to="/signup">
-              <Button variant="inline-link">
-                Sign Up
+              <Button
+                onClick={() => confirmPlaceSelected()}
+                disabled={searchLoc === ""}
+                variant="primary"
+              >
+                Ok
               </Button>
-            </Link>
-            <Link to="/login">
-              <Button variant="inline-link">
-                Log in
+            </div>
+            <div className="autolocate-div">
+              <Button onClick={() => handleFindLocation()}>
+                Find My Location
               </Button>
-            </Link>
-          </>
-        )}
-        <Link to="/createListing">
-          <Button variant="primary">
-            List an item
-          </Button>
-        </Link>
+              <span style={{ marginLeft: "1rem" }}>
+                {loading ? (
+                  <Spinner />
+                ) : (
+                  address && (
+                    <div className="autolocate-result">
+                      {address}
+                      <div id="check-icon" onClick={() => setLocationPref(false)}>
+                        <Icon name="check" variant="success" />
+                      </div>
+                    </div>
+                  )
+                )}
+              </span>
+            </div>
+          </div>
+        </SpaceBetween>
+      </Modal>
+      <div className="navbar">
+        <div className="navbar-l">
+          <Link className="link" to="/">
+            <h3 className="logo-text">Broke Blessings</h3>
+          </Link>
+          <span className="location-pref" onClick={() => setLocationPref(true)}>
+            <LocationSVG></LocationSVG>
+          </span>
+        </div>
+        <div className="navbar-m">
+          <Input
+            onChange={({ detail }) => setSearch(detail.value)}
+            value={search}
+            placeholder="Search"
+          />
+        </div>
+        <div className="navbar-r">
+          {currentUser ? (
+            <>
+              <Button onClick={() => signOut(auth)} variant="inline-link">
+                Sign Out
+              </Button>
+              <Link className="link" to="/user">
+                <Avatar
+                  alt="profile pic"
+                  src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+                />
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/signup">
+                <Button variant="inline-link">Sign Up</Button>
+              </Link>
+              <Link to="/login">
+                <Button variant="inline-link">Log in</Button>
+              </Link>
+            </>
+          )}
+          <Link to="/createListing">
+            <Button variant="primary">List an item</Button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
