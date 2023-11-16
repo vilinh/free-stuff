a1ccaa067cd8f49bf4b7ab2d4845e28beb38fcd1
KimLinhVu <klvbubble@gmail.com>
2023-11-16 10:21:30 -0800

    fix prettier

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,40 +1,40 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
-	{
-		uid: {
-			type: String,
-			required: true,
-		},
-		display_name: {
-			type: String,
-			default: "",
-		},
-		email: {
-			type: String,
-			required: true,
-		},
-		profile_pic: {
-			type: String,
-		},
-		claimed_listings: {
-			type: [String],
-			default: [],
-		},
-		location: {
-			address: {
-				type: String,
-				required: true,
-			},
-			latitude: {
-				type: Number,
-			},
-			longitude: {
-				type: Number,
-			},
-		}
-	},
-	{ collection: "users_list" }
+  {
+    uid: {
+      type: String,
+      required: true,
+    },
+    display_name: {
+      type: String,
+      default: "",
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    profile_pic: {
+      type: String,
+    },
+    claimed_listings: {
+      type: [String],
+      default: [],
+    },
+    location: {
+      address: {
+        type: String,
+        required: true,
+      },
+      latitude: {
+        type: Number,
+      },
+      longitude: {
+        type: Number,
+      },
+    },
+  },
+  { collection: "users_list" },
 );
 
 export default mongoose.model("User", UserSchema);
